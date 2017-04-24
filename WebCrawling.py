14b4d96f1dcaa85b9ba2c1636ded15d938f21787
InbarKar <inbarkarmy@campus.technion.ac.il>
2017-04-24 20:29:06 +0300

    web not workig- pls finish proj by your self beautiful

--- WebCrawling.py
+++ WebCrawling.py
@@ -1,15 +1,30 @@
-import urllib.request
-from lxml.html import HTMLParser
-from lxml import etree
-from io import StringIO, BytesIO
+from google.modules import standard_search
+import requests
+import re
+import random
+import time
+from requests.utils import unquote
+from urllib.parse import urlparse
+with open('movie_titles.txt', 'r') as myfile:
+    movies = myfile.readlines()
+for movie in movies:
+    movie = movie[movie.find(','):]
+    movie = movie[1:]
+    year = movie[0:movie.find(',')]
+    name = movie[movie.find(','):]
+    name = name[1:]
+    print(name)
+    res_search = standard_search.search(name, 1)
+    for s in res_search:
+        link = s.link
+        linkStr = urlparse(link)
+        print(linkStr[1])
+        linkStr = linkStr[1]
+        web_name = linkStr[linkStr.find('.'):linkStr.find('.')]
+        print(web_name)
+        print(s.name)
+        print(s.link)
+    rand = random.uniform(0.1, 5)
+    time.sleep(rand)
 
-IMDB_url = "http://www.imdb.com"
 
-with urllib.request.urlopen(IMDB_url) as url:
-    broken_html = url.read()
-parser = etree.HTMLParser()
-tree = etree.parse(StringIO(broken_html), parser)
-s = tree.getroot()
-print(s)
-result = etree.tostring(tree.getroot(),pretty_print=True, method="html")
-print(result)
