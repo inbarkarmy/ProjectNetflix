b9006fc44d56ef5b099440ed7eda66e44d8dcb21
InbarKar <inbarkarmy@campus.technion.ac.il>
2017-08-15 19:38:43 +0300

    Web Crawling- final adition- line by line
    Signed-off-by: InbarKar <inbarkarmy@campus.technion.ac.il>

--- WebCrawling.py
+++ WebCrawling.py
@@ -7,75 +7,80 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
 
+
 def imdb_pars(imdb_soup):
-        print(imdb_soup.prettify())
-        res = dict()
-        inf = imdb_soup.find("div", attrs={"class": "subtext"})
-        time = inf.find("time")
-        Time = time.text.strip()
-        if Time is None:
-            res['Time'] = ""
-        res['Time'] = Time
-        i = 0
-        for a in inf.findAll("a"):
-            if i == 3:
-                break
-            span = a.find("span", attrs={"itemprop": "genre"})
-            key = "genre" + str(i)
-            if (span):
-                Genre = span.text
-                res[key] = Genre
-                i = i + 1
-        print(i)
-        if i < 3:
-            while i < 3:
-                key = "genre" + str(i)
-                res[key] = ""
-                i = i + 1
-        cred_sums = imdb_soup.findAll("div", attrs={"class": "credit_summary_item"})
-        for cred_sum in cred_sums:
-            h =  cred_sum.find("h4")
-            if h.text == "Stars:":
-                i = 0
-                for actor in cred_sum.findAll("span", attrs={"itemprop": "actors"}):
-                    if i == 3:
-                        break
-                    tmp1 = actor.find("a")
-                    tmp2 = tmp1.find("span", attrs={"class":"itemprop","itemprop": "name"})
-                    Actor = tmp2.text
-                    key = "actor"+str(i)
-                    res[key] = Actor
-                    i = i + 1
-                if i < 3:
-                    while i < 3:
-                        key = "actor" + str(i)
-                        res[key] = ""
-                        i = i + 1
-        txtblocks = imdb_soup.findAll("div", attrs={"class": "txt-block"})
-        for txtblock in txtblocks:
-            fieldType = txtblock.find("h4")
-            if (fieldType):
-                if fieldType.text == "Country:":
-                    country = txtblock.find("a")
-                    if (country):
-                        Country = country.text
-                        res['country'] = Country
-                if fieldType.text == "Language:":
-                    lang = txtblock.find("a")
-                    if (lang):
-                        Lang = lang.text
-                        res['lang'] = Lang
-        recDirectorrecEllipsis = imdb_soup.find("div", attrs={"class": "rec-director rec-ellipsis"})
-        if (recDirectorrecEllipsis):
-            Director = recDirectorrecEllipsis.text[recDirectorrecEllipsis.text.find(":") + 1:]
-            res['director'] = Director.strip()
-        rating = imdb_soup.find("div", attrs={"class": "ratingValue"})
-        if (rating):
-            Rate = rating.text.strip()
-            res['rate'] = Rate
-        for i in res:
-            print(i, res[i])
+    res = dict()
+    inf = imdb_soup.find("div", attrs={"class": "subtext"})
+    if inf is None:
+        res['Time'] = -1
         return res
+    time = inf.find("time")
+    Time = ""
+    if time is not None:
+        Time = time.text.strip()
+    if Time is None:
+        res['Time'] = ""
+    res['Time'] = Time
+    i = 0
+    for a in inf.findAll("a"):
+        if i == 3:
+            break
+        span = a.find("span", attrs={"itemprop": "genre"})
+        key = "genre" + str(i)
+        if (span):
+            Genre = span.text
+            res[key] = Genre
+            i = i + 1
+    print(i)
+    if i < 3:
+        while i < 3:
+            key = "genre" + str(i)
+            res[key] = ""
+            i = i + 1
+    cred_sums = imdb_soup.findAll("div", attrs={"class": "credit_summary_item"})
+    for cred_sum in cred_sums:
+        h =  cred_sum.find("h4")
+        if h.text == "Stars:":
+            i = 0
+            for actor in cred_sum.findAll("span", attrs={"itemprop": "actors"}):
+                if i == 3:
+                    break
+                tmp1 = actor.find("a")
+                tmp2 = tmp1.find("span", attrs={"class":"itemprop","itemprop": "name"})
+                Actor = tmp2.text
+                key = "actor"+str(i)
+                res[key] = Actor
+                i = i + 1
+            if i < 3:
+                while i < 3:
+                    key = "actor" + str(i)
+                    res[key] = ""
+                    i = i + 1
+    txtblocks = imdb_soup.findAll("div", attrs={"class": "txt-block"})
+    for txtblock in txtblocks:
+        fieldType = txtblock.find("h4")
+        if (fieldType):
+            if fieldType.text == "Country:":
+                country = txtblock.find("a")
+                if (country):
+                    Country = country.text
+                    res['country'] = Country
+            if fieldType.text == "Language:":
+                lang = txtblock.find("a")
+                if (lang):
+                    Lang = lang.text
+                    res['lang'] = Lang
+    recDirectorrecEllipsis = imdb_soup.find("div", attrs={"class": "rec-director rec-ellipsis"})
+    if (recDirectorrecEllipsis):
+        Director = recDirectorrecEllipsis.text[recDirectorrecEllipsis.text.find(":") + 1:]
+        res['director'] = Director.strip()
+    rating = imdb_soup.find("div", attrs={"class": "ratingValue"})
+    if (rating):
+        Rate = rating.text.strip()
+        res['rate'] = Rate
+    for i in res:
+        print(i, res[i])
+    return res
 
 def parse_rotten_tomato(rotten_tomatoes_soup):
     res = dict()
@@ -114,7 +119,7 @@ def parse_rotten_tomato(rotten_tomatoes_soup):
         if category_n == "Written By:":
             a = li.find('a')
             Written = a.text.strip()
-            res['written'] = Written
+            #res['written'] = Written
     All = rotten_tomatoes_soup.findAll("section", attrs={"class": "panel panel-rt panel-box "})
     for sec in All:
         category = sec.find("h2", attrs={"class": "panel-heading"})
@@ -157,22 +162,21 @@ def get_results(name, year, j):
         web_name = link[link.find('.') + 1:]
         web_name = web_name[:web_name.find('.')]
         if web_name == "imdb":
-            imdb = requests.get(link)
-            imdb_soup = BeautifulSoup(imdb.text, "html.parser")
-            res = imdb_pars(imdb_soup)
-            j = j+1
-            if res['Time'] is None:
-                return
-            break
+            if link.find("quotes") == -1 and link.find("epcast") == -1:
+                imdb = requests.get(link)
+                imdb_soup = BeautifulSoup(imdb.text, "html.parser")
+                res = imdb_pars(imdb_soup)
+                if res != -1:
+                    j = j+1
+                    break
         if web_name == "rottentomatoes":
-            rotten_tomatoes = requests.get(link)
-            rotten_tomatoes_soup = BeautifulSoup(rotten_tomatoes.text, "html.parser")
-            res = parse_rotten_tomato(rotten_tomatoes_soup)
-            j = j + 1
-            if res['Time'] is None:
-                return
-            break
-
+            if link.find("reviews") == -1 :
+                rotten_tomatoes = requests.get(link)
+                rotten_tomatoes_soup = BeautifulSoup(rotten_tomatoes.text, "html.parser")
+                res = parse_rotten_tomato(rotten_tomatoes_soup)
+                if res != -1:
+                    j = j + 1
+                    break
     if 'Time' not in res.keys():
         res['Time'] = ""
     if 'lang' not in res.keys():
@@ -196,60 +200,38 @@ def get_results(name, year, j):
     if 'genre2' not in res.keys():
         res['genre2'] = ""
 
-    #df = pd.DataFrame({name:[year, res['genre0'],res['genre1'],res['genre2'], res['actor0'],res['actor1'],res['actor2'], res['director'], res['time'], res['country'], res['rate']]})
-    #df.to_csv(r'C:\Users\inbar\Desktop\project\ProjectNetflix\Movies_info.csv')
-    #print(df)
     driver.close()
     return res
 
+
+
 j = 0
-title = []
-year_ = []
-duration = []
-country = []
-director = []
-rate = []
-lang = []
-genre0 =[]
-genre1 =[]
-genre2 =[]
-actor0 = []
-actor1 =[]
-actor2 = []
-df = pd.DataFrame()
+FILE_NAME = 'database.csv'
+
+TITLES_LIST = ['title','year','Time','country','director',
+               'lang','rate','genre0','genre1','genre2',
+              'actor0','actor1','actor2']
+
+inv_titlesDict = dict(enumerate(TITLES_LIST))
+titlesDict = {v:k for k,v in inv_titlesDict.items()}
+
+#with open (FILE_NAME,'a') as fh:
+    #fh.write(','.join(TITLES_LIST)+'\n')
+csvlLine = [0]*len(TITLES_LIST)
 for line in open('movie_titles.txt'):
-    uid, year, name = line.split(',')
+    no_id = line[line.find(',') + 1:]
+    year = no_id[:no_id.find(',')]
+    name = no_id[no_id.find(',') + 1:]
     name = name[:-1]
-    #print(search)
+    print("year ", year, "name: ", name)
     res = get_results(name, year, j)
-    title.append(name)
-    year_.append(year)
-    print(res['Time'])
-    duration.append(res['Time'])
-    country.append(res['country'])
-    director.append(res['director'])
-    rate.append(res['rate'])
-    lang.append(res['lang'])
-    genre0.append(res['genre0'])
-    genre1.append(res['genre1'])
-    genre2.append(res['genre2'])
-    actor0.append(res['actor0'])
-    actor1.append(res['actor1'])
-    actor2.append(res['actor2'])
-print(genre0)
-df['name'] = title
-df['year'] = year_
-df['time'] = duration
-df ['director'] = director
-df ['country'] = country
-df ['lang'] = lang
-df['rate'] = rate
-df['genre0'] = genre0
-df['genre1'] = genre1
-df['genre2'] = genre2
-df['actor0'] = actor0
-df['actor1'] = actor1
-df['actor2'] = actor2
-df.to_csv(r'C:\Users\inbar\Desktop\project\ProjectNetflix\Movies_info.csv')
-print(df)
+    res['title'] = name
+    res['year'] = year
+
+    for key in res:
+        csvlLine[titlesDict[key]] = res[key]
+    csvlLine = [str(x) for x in csvlLine]
+    with open(FILE_NAME, 'a') as fh:
+        fh.write(','.join(csvlLine) + '\n')
+
 print(j)
