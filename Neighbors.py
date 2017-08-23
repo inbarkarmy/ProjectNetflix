2236e988de4d8a41dca2f1d4ed055a6c1b0f5655
orbach <orbach9@gmail.com>
2017-08-23 20:51:39 +0300

    distance matrix Signed-off-by: orbach <orbach9@gmail.com>

--- Neighbors.py
+++ Neighbors.py
@@ -3,34 +3,73 @@ import numpy as numpy
 import pandas as pd
 
 #max distance between movies - define as needed
-maxDist = 10
-def caculateDistance(binaryVec1, binaryVec2 ):
+maxDist = 4
+
+def caculateVecDistance(binaryVec1, binaryVec2 ):
     dist=maxDist
     if len(binaryVec1)==len(binaryVec2):
         for i in range(len(binaryVec1)):
             if binaryVec1[i]==binaryVec2[i]:
                 if binaryVec2[i]==1:
                     dist=dist-1
-    print(dist)
-data = pd.read_csv('Movies_info.csv',encoding='latin1')
-movieCount = 0
-totalGenres = 22
-genreMatrix = numpy.zeros((7,totalGenres))
-movieCount =0
-for row in data["name"]:
-    for i in range(3):
-        curGenre = "genre"+str(i)
-        print(data[curGenre][movieCount])
-        genre = data[curGenre][movieCount]
-        dict={0:'Comedy' ,1:'Action' , 2:'Adventure' , 3:('Animated','Animation'), 4:'Biography', 5:'Crime',
-              6:'Documentary', 7:'Drama',8:'Family', 9:'Fantasy', 10:'Film-Noir', 11:'History',
-              12:'Horror', 13:'Music', 14:'Musical', 15:'Mystery', 16:'Romance',
-              17:('Science Fiction','Sci-Fi'), 18:'Sport', 19:'Thriller', 20:'War', 21:'Western'}
-        for key in range(21):
-            if genre==dict.get(key):
-                genreMatrix[movieCount][key]=1
-    movieCount = movieCount + 1
-print(genreMatrix)
-#example of using caculateDistance - remove
-print(caculateDistance(genreMatrix[4],genreMatrix[5]))
+    return dist
+
+#genreWeghit = 4
+#rateWeight = 2
+#yearWeight = 0.01
+def TwoMoviesDist(genreVec1, rate1, year1, genreVec2, rate2, year2):
+
+    genreDist = caculateVecDistance(genreVec1,genreVec2)
+    #if rate1>rate2:
+    #    rateDist = rate1-rate2
+    #else:
+    #    rateDist = rate2-rate1
+    #if year1 > year2:
+    #    yearDist = year1 - year2
+    #else:
+    #    yearDist = year2 - year1
+    #totalDist = (genreWeghit*genreDist)+(rateWeight*rateDist)+(yearWeight*yearDist)
+    #return totalDist
+    return genreDist
+
+
+def createMovieDistancesMatrix():
+    data = pd.read_csv('Movies_info.csv', encoding='latin1')
+    moviesNum =len(data["name"])
+    genreMatrix = createGenreMatrix()
+    res = numpy.zeros((moviesNum, moviesNum))
+    i=0
+    for movie1 in data["name"]:
+        j=0
+        for movie2 in data["name"]:
+            if i==j:
+                curDist=0
+            else:
+                curDist = TwoMoviesDist(genreMatrix[i],3,1998,genreMatrix[j],3,1998)
+            res[i][j] = curDist
+            j=j+1
+        i=i+1
+    return res
+
+def createGenreMatrix():
+    data = pd.read_csv('Movies_info.csv',encoding='latin1')
+    totalGenres = 22
+    genreMatrix = numpy.zeros((7,totalGenres))
+    movieCount =0
+    for row in data["name"]:
+        for i in range(3):
+            curGenre = "genre"+str(i)
+            genre = data[curGenre][movieCount]
+            dictGenre={0:'Comedy' ,1:'Action' , 2:'Adventure' , 3:('Animated','Animation'), 4:'Biography', 5:'Crime',
+                6:'Documentary', 7:'Drama',8:'Family', 9:'Fantasy', 10:'Film-Noir', 11:'History',
+                12:'Horror', 13:'Music', 14:'Musical', 15:'Mystery', 16:'Romance',
+                17:('Science Fiction','Sci-Fi'), 18:'Sport', 19:'Thriller', 20:'War', 21:'Western'}
+            for key in range(21):
+                if genre==dictGenre.get(key):
+                    genreMatrix[movieCount][key]=1
+        movieCount = movieCount + 1
+    return genreMatrix
+
+
+print(createMovieDistancesMatrix())
 
